9a1ae9a781fe79cf65e14852f8d0cb0d51c28d10
James Brooks <jhbrooks@uchicago.edu>
2016-05-05 09:30:32 -0400

    Add posts index with seed data

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+  default_scope { order(created_at: :desc) }
+
+  validates :content, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "orders from most recent to least recent" do
+    one = create(:post, content: "Test")
+    most_recent = create(:post, created_at: Time.now + 60)
+    two = create(:post)
+
+    expect(Post.first).to eql(most_recent)
+  end
+
+  it "validates content presence" do
+    contentless = build(:post, content: " ")
+
+    expect(contentless.save).to be(false)
+  end
 end
