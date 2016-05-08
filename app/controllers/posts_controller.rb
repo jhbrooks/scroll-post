33f7f4981f775806b728473ede0b5e891baf8aaf
James Brooks <jhbrooks@uchicago.edu>
2016-05-07 20:37:22 -0400

    Fix list item duplication bug caused by scrolling after posting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   def index
     @post = Post.new
+    page = (params[:page] || 1).to_i
+    session[:offset] ||= "0"
+    session[:offset] = "0" if page == 1
     @posts = Post.paginate(page: params[:page], per_page: 20)
+                 .offset((page - 1) * 20 + session[:offset].to_i)
     respond_to do |format|
       format.html
       format.js
@@ -14,7 +18,10 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
           format.html { redirect_to root_path }
-          format.js
+          format.js do
+            session[:offset] ||= "0"
+            session[:offset] = (session[:offset].to_i + 1).to_s
+          end
       else
           format.html { render :index }
           format.js
--- spec/features/infinite_scrolls_spec.rb
+++ spec/features/infinite_scrolls_spec.rb
@@ -13,18 +13,18 @@ RSpec.feature "InfiniteScrolls", type: :feature do
       create_list(:post, 20)
       expect(Post.count > 20).to be(true)
       visit current_path
-      expect(page).to have_css("li", count: 20)
+      expect(page).to have_css("#posts_index li", count: 20)
       page.execute_script("window.scrollTo(0, 100000)")
-      expect(page).to have_css("li", count: 21)
+      expect(page).to have_css("#posts_index li", count: 21)
     end
 
     it "loads more records when resized so that no scroll exists" do
       create_list(:post, 20)
       expect(Post.count > 20).to be(true)
       visit current_path
-      expect(page).to have_css("li", count: 20)
+      expect(page).to have_css("#posts_index li", count: 20)
       page.driver.browser.manage.window.resize_to(1000, 5000)
-      expect(page).to have_css("li", count: 21)
+      expect(page).to have_css("#posts_index li", count: 21)
     end
 
     it "loads more records when no scroll exists to begin with" do
@@ -32,7 +32,7 @@ RSpec.feature "InfiniteScrolls", type: :feature do
       create_list(:post, 20)
       expect(Post.count > 20).to be(true)
       visit current_path
-      expect(page).to have_css("li", count: 21)
+      expect(page).to have_css("#posts_index li", count: 21)
     end
   end
 end
